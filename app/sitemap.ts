1bfbe324b199baf56317762630a4a5da29f3d054
Ashwani Senapati <senapatiashwani47@gmail.com>
2025-10-26 10:36:55 +0530

    chore: added sityemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -4,25 +4,25 @@ import { MetadataRoute } from "next";
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: "https://instcon2026.nitrkl.ac.in/",
+      url: "https://i3-st.vercel.app/",
       lastModified: new Date(),
       changeFrequency: "weekly",
       priority: 1,
     },
     {
-      url: "https://instcon2026.nitrkl.ac.in/call-for-papers",
+      url: "https://i3-st.vercel.app/call-for-papers",
       lastModified: new Date(),
       changeFrequency: "weekly",
       priority: 0.8,
     },
       {
-      url: "https://instcon2026.nitrkl.ac.in/about",
+      url: "https://i3-st.vercel.app/about",
       lastModified: new Date(),
       changeFrequency: "weekly",
       priority: 0.8,
     },
       {
-      url: "https://instcon2026.nitrkl.ac.in/advisory",
+      url: "https://i3-st.vercel.app/advisory",
       lastModified: new Date(),
       changeFrequency: "weekly",
       priority: 0.8,
